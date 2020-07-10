9c0ab7bf711d1ee1b14eaf753ca6ca134c976530
norm <norm@intercom.io>
2020-07-09 20:52:43 -0400

    string svc 2

--- main.go
+++ main.go
@@ -1,110 +1,41 @@
 package main
 
 import (
-	"context"
-	"errors"
-	"strings"
+    "os"
 
-	"github.com/go-kit/kit/endpoint"
-	httptransport "github.com/go-kit/kit/transport/http"
-	"encoding/json"
-	"net/http"
-	"log"
+    "github.com/go-kit/kit/endpoint"
+    "github.com/go-kit/kit/log"
+
+    "net/http"
+
+    httptransport "github.com/go-kit/kit/transport/http"
 )
 
-type uppercaseRequest struct {
-	S string `json:"s"`
-}
-
-type uppercaseResponse struct {
-	V   string `json:"v"`
-	Err string `json:"err,omitempty"` // errors don't JSON-marshal, so we use a string
-}
-
-type countRequest struct {
-	S string `json:"s"`
-}
-
-type countResponse struct {
-	V int `json:"v"`
-}
-
-type StringService interface {
-	Uppercase(string) (string, error)
-	Count(string) int
-}
-
-type stringService struct{}
-
-func (stringService) Uppercase(s string) (string, error) {
-	if s == "" {
-		return "", ErrEmpty
-	}
-	return strings.ToUpper(s), nil
-}
-
-func (stringService) Count(s string) int {
-	return len(s)
-}
-
-// ErrEmpty is returned when input string is empty
-var ErrEmpty = errors.New("Empty string")
-
-func makeUppercaseEndpoint(svc StringService) endpoint.Endpoint {
-	return func(_ context.Context, request interface{}) (interface{}, error) {
-		req := request.(uppercaseRequest)
-		v, err := svc.Uppercase(req.S)
-		if err != nil {
-			return uppercaseResponse{v, err.Error()}, nil
-		}
-		return uppercaseResponse{v, ""}, nil
-	}
-}
-
-func makeCountEndpoint(svc StringService) endpoint.Endpoint {
-	return func(_ context.Context, request interface{}) (interface{}, error) {
-		req := request.(countRequest)
-		v := svc.Count(req.S)
-		return countResponse{v}, nil
-	}
-}
-
 func main() {
-	svc := stringService{}
+    logger := log.NewLogfmtLogger(os.Stderr)
 
-	uppercaseHandler := httptransport.NewServer(
-		makeUppercaseEndpoint(svc),
-		decodeUppercaseRequest,
-		encodeResponse,
-	)
+    var svc StringService
+    svc = stringService{}
+    svc = loggingMiddleware{logger, svc}
 
-	countHandler := httptransport.NewServer(
-		makeCountEndpoint(svc),
-		decodeCountRequest,
-		encodeResponse,
-	)
+    var uppercase endpoint.Endpoint
+    uppercase = makeUppercaseEndpoint(svc)
 
-	http.Handle("/uppercase", uppercaseHandler)
-	http.Handle("/count", countHandler)
-	log.Fatal(http.ListenAndServe(":8080", nil))
-}
+    var count endpoint.Endpoint
+    count = makeCountEndpoint(svc)
 
-func decodeUppercaseRequest(_ context.Context, r *http.Request) (interface{}, error) {
-	var request uppercaseRequest
-	if err := json.NewDecoder(r.Body).Decode(&request); err != nil {
-		return nil, err
-	}
-	return request, nil
-}
+    uppercaseHandler := httptransport.NewServer(
+        uppercase,
+        decodeUppercaseRequest,
+        encodeResponse,
+    )
 
-func decodeCountRequest(_ context.Context, r *http.Request) (interface{}, error) {
-	var request countRequest
-	if err := json.NewDecoder(r.Body).Decode(&request); err != nil {
-		return nil, err
-	}
-	return request, nil
-}
+    countHandler := httptransport.NewServer(
+        count,
+        decodeCountRequest,
+        encodeResponse,
+    )
 
-func encodeResponse(_ context.Context, w http.ResponseWriter, response interface{}) error {
-	return json.NewEncoder(w).Encode(response)
+    http.Handle("/uppercase", uppercaseHandler)
+    http.Handle("/count", countHandler)
 }
